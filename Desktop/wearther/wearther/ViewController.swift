b8dd1d3622fbdc517e6ee8da4c935be8530b9905
Tiasha Joardar <tiashajoardar@wireless-10-146-53-213.public.utexas.edu>
2018-03-20 17:44:42 -0500

    First commit

---
+++ Desktop/wearther/wearther/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wearther
+//
+//  Created by Tiasha Joardar on 3/20/18.
+//  Copyright © 2018 Tiasha Joardar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
