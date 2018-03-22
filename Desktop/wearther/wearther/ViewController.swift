7f329a58bc74ede817b607dc85c3d9f21d8cd4ec
Kevin Zhang <kevinzhang@utexas.edu>
2018-03-22 16:27:13 -0500

    learning github
    Kevin made some comments

--- Desktop/wearther/wearther/ViewController.swift
+++ Desktop/wearther/wearther/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //fake comment code
+    //learning how to use github
+    /*
+        kevin is smart
+    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
