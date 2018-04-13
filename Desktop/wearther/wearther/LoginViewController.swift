b914b6e4db932b3d20ffee3575bfece92738a6c4
Amy Fang <amyfang@utexas.edu>
2018-04-12 19:15:47 -0500

    updated segue

--- Desktop/wearther/wearther/LoginViewController.swift
+++ Desktop/wearther/wearther/LoginViewController.swift
@@ -55,14 +55,14 @@ class LoginViewController: UIViewController {
         self.performSegue(withIdentifier: "register", sender: sender)
     }
     
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "" {
+            print("something")
+        }
     }
-    */
 
 }
