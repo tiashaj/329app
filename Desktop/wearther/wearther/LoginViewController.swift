364319fa0da07f1f330af792fa3c8152f7da52f9
Amy Fang <amyfang@utexas.edu>
2018-04-30 17:39:16 -0500

    did stuff

--- Desktop/wearther/wearther/LoginViewController.swift
+++ Desktop/wearther/wearther/LoginViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-import CoreData
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -25,37 +26,48 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func login(_ sender: Any) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
-        request.returnsObjectsAsFaults = false
-        request.predicate = NSPredicate(format: "email = %@", "" + email.text!)
-        
-        let results:NSArray = try! managedContext.fetch(request) as NSArray
-        
-        if(results.count >= 1){
-            let res = results[0] as! NSManagedObject
-            email.text = res.value(forKey: "email") as? String
-            password.text = res.value(forKey: "password") as? String
+        if self.email.text == "" || self.password.text == "" {
             
-            errorMessage.text = ""
-        }
-        else{
-            errorMessage.text = "Invalid username and password"
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
         }
     }
-   /*
-    func checkForEmailAndPasswordMatch (email:String, password:String) -> Bool {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-    }
-    */
+    
     @IBAction func segueToRegister(_ sender: Any) {
         self.performSegue(withIdentifier: "register", sender: sender)
     }
     
-
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
--- Desktop/wearther/wearther/RegisterViewController.swift
+++ Desktop/wearther/wearther/RegisterViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 import Firebase
 import FirebaseAuth
 
@@ -31,40 +30,32 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func register(_ sender: Any) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        
-        // Create the entity we want to save
-        let entity = NSEntityDescription.entity(forEntityName: "User", in: managedContext)
-        let newUser = NSManagedObject(entity: entity!, insertInto: managedContext)
-        
-        if !(name.text?.isEmpty ?? true) && !(email.text?.isEmpty ?? true) && !(password.text?.isEmpty ?? true) && !(confirmPassword.text?.isEmpty ?? true) {
-            if !isValidPassword(password: password.text!) {
-                errorMessage.text = "Password needs at least 8 characters"
-            }
-            else if !(passwordsMatch(password: password.text!, confirmPassword: confirmPassword.text!)) {
-                errorMessage.text = "Passwords do not match"
-            }
-            else {
-                newUser.setValue(name.text, forKey: "name")
-                newUser.setValue(email.text, forKey: "email")
-                newUser.setValue(password.text, forKey: "password")
-                
-                // Commit changes
-                do {
-                    try managedContext.save()
-                    errorMessage.text = ""
-                    print("User saved")
-                } catch {
-                    // what to do if an error occurs?
-                    let nserror = error as NSError
-                    NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-                    abort()
+        if email.text! == "" || password.text! == "" || !isValidPassword(password: password.text!) || !passwordsMatch(password: password.text!, confirmPassword: confirmPassword.text!) {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
                 }
             }
-        }
-        else {
-            errorMessage.text = "All fields need to be filled"
         }
     }
     
