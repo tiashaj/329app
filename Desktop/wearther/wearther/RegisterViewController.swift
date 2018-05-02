75112c20ab80adaa890e36f656d3e455eae383c3
Amy Fang <amyfang@utexas.edu>
2018-05-02 00:37:27 -0500

    profile pics work

--- Desktop/wearther/wearther/RegisterViewController.swift
+++ Desktop/wearther/wearther/RegisterViewController.swift
@@ -7,25 +7,70 @@
 //
 
 import UIKit
-import Firebase
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var confirmPassword: UITextField!
     @IBOutlet weak var errorMessage: UILabel!
-    
     var refUsers: DatabaseReference!
+    
+    lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "profile_icon")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
+        imageView.isUserInteractionEnabled = true
+        
+        return imageView
+    }()
+    
+    @objc func handleSelectProfileImageView() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            profileImageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupProfileImageView() {
+        //need x, y, width, height constraints
+        profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImageView.bottomAnchor.constraint(equalTo: name.topAnchor, constant: -20).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(profileImageView)
+        setupProfileImageView()
+        
         password.isSecureTextEntry = true
         confirmPassword.isSecureTextEntry = true
-        
         refUsers = Database.database().reference()
     }
 
@@ -41,11 +86,7 @@ class RegisterViewController: UIViewController {
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
                 
-            let alertWindow = UIWindow(frame: UIScreen.main.bounds)
-            alertWindow.rootViewController = UIViewController()
-            alertWindow.windowLevel = UIWindowLevelAlert
-            alertWindow.makeKeyAndVisible()
-            alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
+            displayAlert(alertController: alertController)
         }
         else if !isValidPassword(password: password.text!) {
             let alertController = UIAlertController(title: "Error", message: "Please enter a password with at least 8 characters", preferredStyle: .alert)
@@ -53,11 +94,7 @@ class RegisterViewController: UIViewController {
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
             
-            let alertWindow = UIWindow(frame: UIScreen.main.bounds)
-            alertWindow.rootViewController = UIViewController()
-            alertWindow.windowLevel = UIWindowLevelAlert
-            alertWindow.makeKeyAndVisible()
-            alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
+            displayAlert(alertController: alertController)
         }
         else if !passwordsMatch(password: password.text!, confirmPassword: confirmPassword.text!) {
             let alertController = UIAlertController(title: "Error", message: "Passwords do not match", preferredStyle: .alert)
@@ -65,22 +102,33 @@ class RegisterViewController: UIViewController {
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
             
-            let alertWindow = UIWindow(frame: UIScreen.main.bounds)
-            alertWindow.rootViewController = UIViewController()
-            alertWindow.windowLevel = UIWindowLevelAlert
-            alertWindow.makeKeyAndVisible()
-            alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
+            displayAlert(alertController: alertController)
         } else {
             Auth.auth().createUser(withEmail: email.text!, password: password.text!, completion: { (user, error) in
                 if error == nil {
-                    print("You have successfully signed up")
                     
                     let uid = user?.uid
-                    // add user to Firebase Database
-                    let user = ["email": self.email.text!,
-                                "name": self.name.text!]
                     
-                    self.refUsers.child("Users").child(uid!).setValue(user)
+                    // Successfully authenticated user
+                    let imageName = UUID().uuidString
+                    let storageRef = Storage.storage().reference().child("profile_images").child("\(imageName).png")
+                    
+                    if let uploadData = UIImagePNGRepresentation(self.profileImageView.image!){
+                        storageRef.putData(uploadData, metadata: nil, completion: { (metadata, error) in
+                            if let error = error {
+                                print(error)
+                                return
+                            }
+                            
+                            if let profileImageURL = metadata?.downloadURL()?.absoluteString {
+                                let user = ["email": self.email.text!,
+                                            "name": self.name.text!,
+                                            "profileImageURL": profileImageURL]
+                                
+                                self.refUsers.child("Users").child(uid!).setValue(user)
+                            }
+                        })
+                    }
                     
                     //Goes to the Home page
                     self.performSegue(withIdentifier: "home", sender: self)
@@ -91,14 +139,18 @@ class RegisterViewController: UIViewController {
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                     alertController.addAction(defaultAction)
                         
-                    let alertWindow = UIWindow(frame: UIScreen.main.bounds)
-                    alertWindow.rootViewController = UIViewController()
-                    alertWindow.windowLevel = UIWindowLevelAlert
-                    alertWindow.makeKeyAndVisible()
-                    alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
+                    self.displayAlert(alertController: alertController)
                 }
             })
         }
+    }
+    
+    func displayAlert(alertController: UIAlertController) {
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindowLevelAlert
+        alertWindow.makeKeyAndVisible()
+        alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
     }
     
     func isValidPassword (password:String) -> Bool {
