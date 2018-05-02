85cdb9d5078d4e863a86d29bffd3b81674f4b933
Tiasha Joardar <tiasha@utexas.edu>
2018-05-02 17:45:10 -0500

    notification update

--- Desktop/wearther/wearther/NotificationViewController.swift
+++ Desktop/wearther/wearther/NotificationViewController.swift
@@ -7,63 +7,22 @@
 //
 
 import UIKit
-import UserNotifications
 
 class NotificationViewController: UIViewController {
     
-    func repeatNotification(){
-        let content = UNMutableNotificationContent()
-        content.title = "Get an Outfit!"
-        content.body = "Let WearTher generate your outfit for today"
-        content.categoryIdentifier = "outfit.reminder.category"
-        
-        var dateComponents = DateComponents()
-        //Every Monday at 11:30AM
-        //dateComponents.hour = 11
-        //dateComponents.minute = 30
-        //dateComponents.weekday = 2
-        dateComponents.second = 0
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        
-        let request = UNNotificationRequest(identifier: "outfit.reminder", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if let error = error {
-                print("error in pizza reminder: \(error.localizedDescription)")
-            }
-        }
-        print("added notification:\(request.identifier)")
-        
-    }
+    @IBOutlet weak var dayField: UITextField!
+    
+    @IBOutlet weak var hourField: UITextField!
+    
+    @IBOutlet weak var minField: UITextField!
     
     
-    var notificationGranted = false
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        UNUserNotificationCenter.current().requestAuthorization(
-        options: [.alert,.sound]){
-            (granted, error) in
-            self.notificationGranted = granted
-            if let error = error {
-                print("granted, but Error in notification permission:\(error.localizedDescription)")
-            }
-        }
+
+        // Do any additional setup after loading the view.
     }
-    
-    @IBAction func addNotification(_ sender: Any) {
-        if notificationGranted{
-            repeatNotification()
-        }else{
-            print("notification not granted")
-        }
-    }
-    
-    @IBAction func showNotification(_ sender: Any) {
-    }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -82,3 +41,4 @@ class NotificationViewController: UIViewController {
     */
 
 }
+
