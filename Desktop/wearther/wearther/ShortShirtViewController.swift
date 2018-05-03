6194e4539529ea0d51b54eabdc31445694fbff93
Tiasha Joardar <tiasha@utexas.edu>
2018-05-02 19:58:28 -0500

    idk

--- Desktop/wearther/wearther/ShortShirtViewController.swift
+++ Desktop/wearther/wearther/ShortShirtViewController.swift
@@ -1,28 +1,39 @@
-//
-//  ShortShirtViewController.swift
-//  wearther
-//
-//  Created by Tiasha Joardar on 5/1/18.
-//  Copyright © 2018 Amy Fang. All rights reserved.
-//
-
-import UIKit
-import Firebase
-import FirebaseAuth
 import FirebaseDatabase
 
-class ShortShirtViewController: UIViewController {
+class ShortShirtViewController: UIViewController, UICollectionViewDataSource {
+    var images = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadImages()
+    }
+    
+    func loadImages(){
+        images.append(UIImage(named: "tshirt.png"))
+        images.append(UIImage(named: "tshirt.png"))
+        images.append(UIImage(named: "tshirt.png"))
+        images.append(UIImage(named: "tshirt.png"))
+        
+        self.imageCollection.reloadData()
     }
     
     @IBAction func onXOut(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = imageCollection.dequeueReusableCell(withReuseIdentifier: "Cell", for:indexPath) as! PicsCollectionViewCell
+        let image = images[indexPath.row]
+        cell.imageView.image = image
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
 }
